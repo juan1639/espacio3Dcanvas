4e72581213837f43c32e4bbf86c2db9bafa5426e
juan <juaneguiabad1639@gmail.com>
2023-12-11 13:22:34 +0100

    agregado audio y mejorado rendimiento dibujando rectangulos

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const constante = {
-	FPS: 60,
+	FPS: 100,
 	TILE_X: 50,
 	TILE_Y: 50,
 	COLUMNAS: 16,
@@ -16,41 +16,50 @@ const constante = {
 
 const resolucion = [constante.TILE_X * constante.COLUMNAS, constante.TILE_Y * constante.FILAS]
 
+const lista_colores = [
+	'white', 'white', 'white',
+	'lightblue', 'lightskyblue', 'cyan',
+	'lightyellow', 'lightyellow', 'lightyellow'
+];
+
 const objeto = {
 	estrella: [],
+	estrellaFija: []
 }
 
-const controles = {
-	izquierda: false,
-	derecha: false,
-	arriba: false,
-	abajo: false
+const sonidos = {
+	musica_fondo: new Audio('./audio/backgroundMusic.wav')
 }
 
 // ============================================================================
-document.addEventListener('keydown', (ev) => {
+class EstrellaFija {
 
-	console.log(ev.key);
+	constructor() {
 
-	if (ev.key === 'ArrowLeft') {
-		controles.izquierda = true;
+		const radio = numero_random(1, 3);
+		const opaca = numero_random(0, 6);
 
-	} else if (ev.key === 'ArrowRight') {
-		controles.derecha = true;
+		this.rect = {
+			x: Math.floor(Math.random()* resolucion[0]),
+			y: Math.floor(Math.random()* resolucion[1]),
+			ancho: radio,
+			alto: radio,
+			alpha: opaca / 10 + 0.5  
+		}
+
+		// console.log(this.rect.alpha, this.rect.ancho);
+
+		const color_rnd = Math.floor(Math.random()* lista_colores.length);
+		this.color = lista_colores[color_rnd];
 	}
-});
 
-document.addEventListener('keyup', (ev) => {
+	dibuja() {
 
-	console.log(ev.key);
-
-	if (ev.key === 'ArrowLeft') {
-		controles.izquierda = false;
-
-	} else if (ev.key === 'ArrowRight') {
-		controles.derecha = false;
+		ctx.globalAlpha = this.rect.alpha;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.rect.x, this.rect.y, this.rect.ancho, this.rect.alto);
 	}
-});
+}
 
 // ============================================================================
 class Estrella {
@@ -78,15 +87,15 @@ class Estrella {
 	dibuja() {
 
 		this.actualiza();
-
-		ctx.beginPath();
+		
+		const x = Math.floor(this.rect.x);
+		const y = Math.floor(this.rect.y);
+		const ancho = Math.floor(this.rect.ancho);
+		const alto = Math.floor(this.rect.alto);
 
 		ctx.globalAlpha = this.move.alpha;
 		ctx.fillStyle = this.color;
-		ctx.arc(this.rect.x, this.rect.y, this.rect.ancho, 0, 2 * Math.PI);
-		ctx.fill();
-
-		ctx.closePath();
+		ctx.fillRect(x, y, ancho, alto);
 	}
 
 	actualiza() {
@@ -94,7 +103,8 @@ class Estrella {
 		this.rect.x += Math.cos(this.move.angulo) * this.move.vel;
 		this.rect.y += Math.sin(this.move.angulo) * this.move.vel;
 
-		this.rect.ancho += this.move.vel / 200;
+		this.rect.ancho += this.move.vel / 100;
+		this.rect.alto = this.rect.ancho;
 		this.move.alpha += this.move.vel / 2000;
 
 		this.check_resetEstrella(false);
@@ -107,31 +117,43 @@ class Estrella {
 
 		if (this.rect.x + ancho > resolucion[0] || this.rect.x < -ancho || this.rect.y + alto > resolucion[1] || this.rect.y < -alto || inicial) {
 
-			const grados_rnd = this.numero_random(0, 359);
+			const grados_rnd = numero_random(0, 359);
 
-			const lista_colores = [
-				'white', 'white', 'white',
-				'lightblue', 'lightskyblue', 'lightskyblue',
-				'lightyellow', 'lightyellow', 'lightblue'
-			];
-
-			this.rect.x = this.numero_random(Math.floor(canvas.width / 2) - 2, Math.floor(canvas.width / 2) + 2);
-			this.rect.y = this.numero_random(Math.floor(canvas.height / 2) - 2, Math.floor(canvas.height / 2) + 2);
+			this.rect.x = numero_random(Math.floor(canvas.width / 2) - 2, Math.floor(canvas.width / 2) + 2);
+			this.rect.y = numero_random(Math.floor(canvas.height / 2) - 2, Math.floor(canvas.height / 2) + 2);
 			this.rect.ancho = 1.0;
 			this.rect.alto = 1.0;
 
 			this.move.angulo = (grados_rnd * Math.PI) / 180;
-			this.move.vel = this.numero_random(1, 4);
+			this.move.vel = numero_random(1, 4);
 			this.move.alpha = 0.4;
 
 			const index = Math.floor(Math.random()* lista_colores.length);
 			this.color = lista_colores[index];
 		}
 	}
+}
 
-	numero_random(min, max) {
-		return Math.floor(Math.random()* (max - min) + min);
-	}
+// ============================================================================
+function numero_random(min, max) {
+	return Math.floor(Math.random()* (max - min) + min);
+}
+
+// ============================================================================
+function dibuja_texto(txt, x, y, font, align, color, alpha) {
+
+	ctx.save();
+
+	ctx.font = font;
+	ctx.textAlign = align;
+	ctx.shadowColor = 'white';
+	ctx.shadowBlur = 8;
+	// ctx.fillStyle = lista_colores[Math.floor(Math.random()* lista_colores.length)];
+	ctx.fillStyle = color;
+	ctx.globalAlpha = alpha;
+	ctx.fillText(txt, x, y);
+
+	ctx.restore();
 }
 
 // ============================================================================
@@ -142,10 +164,12 @@ window.onload = () => {
 
 	ctx.scale(1, 1);
 
+	sonidos.musica_fondo.play();
+	sonidos.musica_fondo.loop = true;
+	
 	// -------------------------------------------------
-	//objeto.jugador.push(new Jugador(pos_inicial.jugador[0], pos_inicial.jugador[1]));
-
 	for (let i = 0; i < constante.NRO_ESTRELLAS; i ++) {
+		objeto.estrellaFija.push(new EstrellaFija());
 		objeto.estrella.push(new Estrella());
 	}
 
@@ -158,9 +182,15 @@ window.onload = () => {
 function bucle_principal() {
 
 	ctx.fillStyle = 'rgb(0, 0, 35)';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillRect(0, 0, resolucion[0], resolucion[1]);
+
+	objeto.estrellaFija.forEach(star => {
+		star.dibuja();
+	});
 
 	objeto.estrella.forEach(star => {
 		star.dibuja();
 	});
+
+	dibuja_texto('Centro IMI', Math.floor(resolucion[0] / 2), resolucion[1] - 30, '24px arial', 'center', 'lightyellow', 1);
 }
